c9d249993cac2bdb85b5f3b9c7ad60dcf275b3fb
SrPampa <raulpampa2@gmail.com>
2022-06-09 21:00:54 +0200

    añadidos servlets de ruth

---
+++ src/main/java/com/venancio/dam/proyectoweb/MostrarEvaluacionesServlet.java
@@ -0,0 +1,42 @@
+package com.venancio.dam.proyectoweb;
+
+import java.io.IOException;
+import java.util.Set;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import com.venancio.dam.proyectoweb.model.Evaluacion;
+import com.venancio.dam.proyectoweb.service.Service;
+
+public class MostrarEvaluacionesServlet extends GenericServlet {
+
+private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		this.doGet(request, response);
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+  
+    	super.doGet(request, response);
+
+		WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+
+		Service service = new Service();
+		Set<Evaluacion> allNotas = service.mostrarNotas();
+
+		ctx.setVariable("alumnos", allNotas);
+
+		TemplateEngine engine = configThymeleaf.getTemplateEngine();
+		engine.process("mostrar_alumnos", ctx, response.getWriter());
+	}
+}
